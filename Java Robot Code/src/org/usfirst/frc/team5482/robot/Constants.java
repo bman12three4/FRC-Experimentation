12f64812f5ada0671e4b0eee2f5c0d5ac25d1e70
Byron Lathi <byron.lathi@student.lovett.org>
2016-11-29 12:11:52 -0500

    Added network info to Constants

--- Java Robot Code/src/org/usfirst/frc/team5482/robot/Constants.java
+++ Java Robot Code/src/org/usfirst/frc/team5482/robot/Constants.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5482.robot;
 
 public class Constants {
-	private static String piAddress = "10.54.82.3";
+	public static String piAddress = "10.54.82.3";
+	public static int piPort = 5482;
 }
